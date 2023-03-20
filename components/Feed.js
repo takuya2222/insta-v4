ba804e4e81251b6d614aa87eabe106656be0e7af
takuya <hukujuth@gmail.com>
2023-03-20 11:00:36 +0900

    Populate fake data using minifaker and create feed and story components

---
+++ components/Feed.js
@@ -0,0 +1,17 @@
+import Stories from "./Stories";
+
+export default function Feed() {
+  return (
+    <main>
+      <section>
+        {/* Stories */}
+        <Stories />
+        {/* Post */}
+      </section>
+      <section>
+        {/* Mini Profile */}
+        {/* Suggestions */}
+      </section>
+    </main>
+  );
+}
