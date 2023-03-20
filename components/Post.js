132f7725635500931246399bd18582bb716cb6c6
takuya <hukujuth@gmail.com>
2023-03-20 14:57:48 +0900

    Create the header and image sections of the post component

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { DotsHorizontalIcon } from "@heroicons/react/solid";
 
 export default function Post({ img, userImg, caption, username, id }) {
-  return <div>
-    <h1>{username}</h1>
-  </div>;
+  return (
+    <div className="bg-white my-7 border rounded-md">
+      {/* Post header */}
+      <div className="flex items-center p-5">
+        <img
+          className="h-12 rounded-full object-cover border p-1 mr-3"
+          src={userImg}
+          alt={username}
+        />
+        <p className="font-bold flex-1 //この要素が大きくなり右のものを右一杯に押し出す">
+          {username}
+        </p>
+        <DotsHorizontalIcon className="h-5" />
+      </div>
+
+      {/* Post Image */}
+      <img className="object-cover w-full" src={img}  />
+    </div>
+  );
 }
--- components/Posts.js
+++ components/Posts.js
@@ -8,7 +8,7 @@ export default function Posts() {
       username: "codewithsahand",
       userImg:
         "https://thumb.ac-illust.com/53/53fc958478b1241da0180a5681338a1b_t.jpeg",
-      img: "https://unsplash.com/ja/%E5%86%99%E7%9C%9F/75aKwQ9Z1Lg",
+      img: "https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
       caption: "Nice picture",
     },
     {
@@ -16,7 +16,7 @@ export default function Posts() {
       username: "takuya",
       userImg:
         "https://thumb.ac-illust.com/53/53fc958478b1241da0180a5681338a1b_t.jpeg",
-      img: "https://unsplash.com/ja/%E5%86%99%E7%9C%9F/cFkg3uxK9CQ",
+      img: "https://images.unsplash.com/photo-1619050405558-b5f39da6eef8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80",
       caption: "New picture from my city",
     },
   ];
