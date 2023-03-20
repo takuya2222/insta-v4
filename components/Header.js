206b23536dc32d484e67c712d99d245e57fd3c13
takuya <hukujuth@gmail.com>
2023-03-20 09:29:36 +0900

    Add the menu and profile image at the right side of the header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import React from "react";
-import { SearchIcon } from "@heroicons/react/outline";
+import { SearchIcon, PlusCircleIcon } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between max-w-6xl">
+    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto //これ大切 ">
       {/* Left */}
       <div className="cursor-pointer w-24 h-24 relative hidden lg:inline-grid">
         <Image
@@ -27,12 +28,24 @@ export default function Header() {
         <div className="absolute top-2 left-2">
           <SearchIcon className="h-5 text-gray-500" />
         </div>
-        <input type="text" placeholder="Search" className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md
-        " />
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md
+        "
+        />
       </div>
 
       {/* Right */}
-      <h1>Right sides</h1>
+      <div className="flex space-x-4 items-center">
+        <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+        <img
+          src="https://thumb.ac-illust.com/53/53fc958478b1241da0180a5681338a1b_t.jpeg"
+          alt="user-icon"
+          className="h-10 rounded-full cursor-pointer"
+        />
+      </div>
     </div>
   );
 }
