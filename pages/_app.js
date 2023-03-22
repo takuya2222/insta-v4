9520bc1e23625580191a044b903bbe4b9dcdc476
takuya <hukujuth@gmail.com>
2023-03-22 15:14:15 +0900

    Get the session and modify the header component with google data

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '../styles/globals.css'
+//このファイルが全てのファイルに影響を及ぼす
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
