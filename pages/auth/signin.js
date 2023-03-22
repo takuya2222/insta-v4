58c42ea735c1bfba096172dc4135114f755b45f9
takuya <hukujuth@gmail.com>
2023-03-22 09:53:44 +0900

    Complete the signin page

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,7 +1,39 @@
-import { getProviders, signIn } from "next-auth";
+import Header from "../../components/Header";
+import { getProviders, signIn } from "next-auth/react";
 
 export default function signin({ providers }) {
-  return <div>signin page</div>;
+  return (
+    <>
+      <Header />
+      <div className="flex justify-center space-x-7 mt-20">
+        <img
+          className="hidden object-cover rotate-6 md:inline-flex mg:w-4"
+          src="https://superviral.com.au/wp-content/uploads/2021/08/instagix-banner-graphic.png"
+          alt="instagram-image"
+        />
+        <div>
+          {Object.values(providers).map((providers) => (
+            <div key={providers.name} className="flex flex-col items-center">
+              <img
+                className="w-32 object-cover"
+                src="https://socodigital.com/wp-content/uploads/2021/03/Instagram.png"
+                alt=""
+              />
+              <p className="text-sm italic my-10 text-center">
+                This app is created for learning purposes
+              </p>
+              <button
+                onClick={() => signIn(providers.id, { callbackUrl: "/" })}
+                className="bg-red-400 rounded-lg p-3 text-white hover:bg-red-500"
+              >
+                Sign in with {providers.name}
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export async function getServerSideProps(context) {
