4871bcd8ed395945b32bca4721d5dd696c0501c1
takuya <hukujuth@gmail.com>
2023-03-22 23:09:16 +0900

    Modify mini profile and story components to include the session

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -11,9 +11,20 @@ export const authOptions = {
     // ...add more providers here
   ],
 
-  pages:{
-    signIn:"/auth/signin"
-  }
-}
+  pages: {
+    signIn: "/auth/signin",
+  },
 
-export default NextAuth(authOptions)
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.username = session.user.name
+        .split("")
+        .join("")
+        .toLocaleLowerCase();
+      session.user.uid = token.sub;
+      return session;
+    },
+  },
+};
+
+export default NextAuth(authOptions);
