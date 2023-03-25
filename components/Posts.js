1badc23c7a5a025b3de09702fd087268b22d83a6
takuya <hukujuth@gmail.com>
2023-03-25 13:09:03 +0900

    Get the post data from database and show it in the feed section

--- components/Posts.js
+++ components/Posts.js
@@ -1,34 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase";
 
 export default function Posts() {
-  const posts = [
-    {
-      id: "1",
-      username: "codewithsahand",
-      userImg:
-        "https://thumb.ac-illust.com/53/53fc958478b1241da0180a5681338a1b_t.jpeg",
-      img: "https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-      caption: "Nice picture",
-    },
-    {
-      id: "2",
-      username: "takuya",
-      userImg:
-        "https://thumb.ac-illust.com/53/53fc958478b1241da0180a5681338a1b_t.jpeg",
-      img: "https://images.unsplash.com/photo-1619050405558-b5f39da6eef8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80",
-      caption: "New picture from my city",
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, "posts"), orderBy("timestamp", "desc")),
+      (snapshot) => {
+        setPosts(snapshot.docs);
+      }
+    );
+    return unsubscribe;
+  });
   return (
     <div>
       {posts.map((post) => (
         <Post
           key={post.id}
           id={post.id}
-          username={post.username}
-          userImg={post.userImg}
-          img={post.img}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          img={post.data().image}
           caption={post.caption}
         />
       ))}
