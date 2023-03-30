c59b434ee9bfbd2a2b8766db2419737726e28451
takuya <hukujuth@gmail.com>
2023-03-31 07:59:32 +0900

    微調整

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,7 +1,17 @@
 import Header from "../../components/Header";
-import { getProviders, signIn } from "next-auth/react";
-
-export default function signin({ providers }) {
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { db } from "../../firebase";
+//firebase-authの実装では頭文字を大文字にした
+export default function Signin() {
+  async function onGoogleClick() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      //googleのログイン画面が現れる
+      await signInWithPopup(auth, provider);
+      console.log(auth);
+    } catch (error) {}
+  }
   return (
     <>
       <Header />
@@ -12,33 +22,24 @@ export default function signin({ providers }) {
           alt="instagram-image"
         />
         <div>
-          {Object.values(providers).map((providers) => (
-            <div key={providers.name} className="flex flex-col items-center">
-              <img
-                className="w-32 object-cover"
-                src="https://socodigital.com/wp-content/uploads/2021/03/Instagram.png"
-                alt=""
-              />
-              <p className="text-sm italic my-10 text-center">
-                This app is created for learning purposes
-              </p>
-              <button
-                onClick={() => signIn(providers.id, { callbackUrl: "/" })}
-                className="bg-red-400 rounded-lg p-3 text-white hover:bg-red-500"
-              >
-                Sign in with {providers.name}
-              </button>
-            </div>
-          ))}
+          <div className="flex flex-col items-center">
+            <img
+              className="w-32 object-cover"
+              src="https://socodigital.com/wp-content/uploads/2021/03/Instagram.png"
+              alt=""
+            />
+            <p className="text-sm italic my-10 text-center">
+              This app is created for learning purposes
+            </p>
+            <button
+              onClick={onGoogleClick}
+              className="bg-red-400 rounded-lg p-3 text-white hover:bg-red-500"
+            >
+              Sign in with Google
+            </button>
+          </div>
         </div>
       </div>
     </>
   );
-}
-
-export async function getServerSideProps(context) {
-  const providers = await getProviders();
-  return {
-    props: { providers },
-  };
 }
